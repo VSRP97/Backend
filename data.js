4eff3c67d6720079642a25d8935292756a45917c
Carlos Andres Venencia Sayas <cvenencia@uninorte.edu.co>
2022-03-10 20:06:01 -0500

    added data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+    player_characters: [
+        {
+            id: 1,
+            name: "play_char1",
+            stats: "pro",
+            level: 5,
+            title: "noob",
+            model: "a model",
+            player: "idk"
+        },
+        {
+            id: 2,
+            name: "play_char2",
+            stats: "noob",
+            level: 10,
+            title: "pro",
+            model: "a different model",
+            player: "idk x2"
+        }
+    ],
+    player_character_index: 2
+}
